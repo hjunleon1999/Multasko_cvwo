249e4ce3d5c169c806f05239a83f20e5f768e9a1
hjunleon1999 <62840119+hjunleon1999@users.noreply.github.com>
2020-12-30 11:22:21 +0800

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,8 +62,8 @@ Rails.application.configure do
       address:              'smtp.gmail.com',
       port:                 587,
       domain:               'example.com',
-      user_name:            'miaomiaox123',
-      password:             'duaolahggqaqjwvd',
+      user_name:            
+      password:             
       authentication:       'plain',
       enable_starttls_auto: true 
     }
